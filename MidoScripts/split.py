6bd87c307effe38915e95d9a555980a8046749a5
maxiplopez@gmail.com <maxiplopez@gmail.com>
2020-11-23 14:09:27 +0100

    Added folder for Mido script

---
+++ MidoScripts/split.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 23 13:58:17 2020
+
+@author: max
+
+Trying to split a dataset using the mido library
+"""
+
+import mido
+
+parentDirectory = ""
+outputDirectory = ""
+files={}
+
