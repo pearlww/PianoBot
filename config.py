1a02101247ddb33a6e658f7633fc0a348d7a4671
maxiplopez@gmail.com <maxiplopez@gmail.com>
2020-12-07 16:30:06 +0100

    config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ device = torch.device('cpu')
 
 # train
 epochs = 100 # 100 for debug
-batch_size = 2 # 2 for debag
+batch_size = 8 # 2 for debag
 dropout = 0.1
 debug = 'true'
 l_r = 0.001
@@ -19,7 +19,7 @@ label_smooth = 0.1
 experiment = 'embedding256-layer6'
 max_seq = 2048
 embedding_dim = 256
-num_layers = 2
+num_layers = 6
 event_dim = 388
 
 pad_token = event_dim
@@ -27,7 +27,7 @@ token_sos = event_dim + 1
 token_eos = event_dim + 2
 vocab_size = event_dim + 3
 
-model_dir = "."
+model_dir = "./train"
 
 # experiment: 'embedding512-layer6'
 # max_seq: 2048
